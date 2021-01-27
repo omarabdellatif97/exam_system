22e32d3a41e8c6e5ebe7cbcf769d4af5151916d9
Omar Hesham <omar_abdellatif97@hotmail.com>
2021-01-27 08:54:48 +0200

    Restructuring

---
+++ ExamSystem.DAL/Models/ChoQue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ExamSystem.DAL.Models
+{
+    public partial class ChoQue
+    {
+        public int ChoId { get; set; }
+        public int QueId { get; set; }
+
+        public virtual Choice Cho { get; set; }
+        public virtual Question Que { get; set; }
+    }
+}
