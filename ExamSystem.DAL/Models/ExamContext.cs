8240d797610b9d64ceb4424b3e2f25046b405d46
Omar Hesham <omar_abdellatif97@hotmail.com>
2021-05-11 14:50:01 +0200

    Modifying connection string

--- ExamSystem.DAL/Models/ExamContext.cs
+++ ExamSystem.DAL/Models/ExamContext.cs
@@ -36,7 +36,7 @@ namespace ExamSystem.DAL.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server=examdatabaseproject.database.windows.net;Database=Exam;User Id=ITI_Exam;Password=db_project2021;");
+                optionsBuilder.UseSqlServer("server=.;Database=Exam;Integrated Security=true");
             }
         }
 
